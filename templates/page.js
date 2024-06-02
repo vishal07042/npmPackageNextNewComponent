d972f979e1b44ddc7045829266b4f50cb9aae085
vishal mehta <professionalprovishal@gmail.com>
2024-06-02 12:05:24 +0530

    mvp implemented implemented

--- templates/page.js
+++ templates/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function COMPONENT_NAME() {
-  return <div> this is {COMPONENT_NAME} </div>;
+  return <div> this is COMPONENT_NAME </div>;
 }
 
 export default COMPONENT_NAME;
